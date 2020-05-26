da2a16e5e6e5bfa0c4e155c21476a4e7160844c6
Rachel V. Thomas-Coleman <rachel.v.thomas7185@gmail.com>
2020-05-25 22:43:07 -0400

    It says weh

--- discord_bot.py
+++ discord_bot.py
@@ -3,11 +3,19 @@ import discord
 import os
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD_ID')
+CHANNEL = os.getenv('DISCORD_CHANNEL_ID')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    #get the weh guild
+    guild = client.get_guild(int(GUILD))
+    #get the bot testing channel
+    channel = guild.get_channel(int(CHANNEL))
+    #send message to the channel
+    await channel.send('weh')
 
 client.run(TOKEN)
