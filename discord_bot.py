fdb08debcb9536fa579a4c932004aa98f981f9e3
Rachel V. Thomas-Coleman <rachel.v.thomas7185@gmail.com>
2020-07-12 19:56:33 -0400

    It can start and stop the server.

--- discord_bot.py
+++ discord_bot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import boto3
 import discord
 import os
 
@@ -6,6 +7,8 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD_ID')
 CHANNEL = os.getenv('DISCORD_CHANNEL_ID')
 MC_ADMIN_ROLE = os.getenv('DISCORD_ADMIN_ROLE_ID')
+MINECRAFT_INSTANCE_ID = os.getenv('MINECRAFT_INSTANCE_ID')
+REGION = os.getenv('AWS_REGION')
 
 client = discord.Client()
 
@@ -44,7 +47,35 @@ async def start_server(client, message):
     #check if message author is in list of admins
     if(await is_admin(client, message.author)):
         #if yes, start server
-        await message.channel.send('weh would start server')
+        
+        #make an ec2 client
+        ec2 = boto3.client('ec2', region_name = REGION)
+
+        #get the JSON response from starting the minecraft instance
+        response = ec2.start_instances(
+            InstanceIds = [MINECRAFT_INSTANCE_ID]
+        )
+
+        #get the status of the instance from the response
+        #resopnse structure:
+        #{
+        #   'StartingInstances': [
+        #        {
+        #            'CurrentState': {
+        #                'Code': 123,
+        #                'Name': 'pending'|'running'|'shutting-down'|'terminated'|'stopping'|'stopped'
+        #            },
+        #            'InstanceId': 'string',
+        #            'PreviousState': {
+        #                'Code': 123,
+        #                'Name': 'pending'|'running'|'shutting-down'|'terminated'|'stopping'|'stopped'
+        #            }
+        #        },
+        #    ]
+        #}
+        status = response['StartingInstances'][0]['CurrentState']['Name']
+
+        await message.channel.send(f'new server status: {status}, weh')
     else:
         await message.channel.send('you are not an admin, weh')
 
@@ -52,7 +83,35 @@ async def stop_server(client, message):
     #check if message author is in list of admins
     if(await is_admin(client, message.author)):
         #if yes, stop server
-        await message.channel.send('weh would stop server')
+        
+        #make an ec2 client
+        ec2 = boto3.client('ec2', region_name = REGION)
+
+        #get the JSON response from stopping the minecraft instance
+        response = ec2.stop_instances(
+            InstanceIds = [MINECRAFT_INSTANCE_ID]
+        )
+
+        #get the status of the instance from the response
+        #resopnse structure:
+        #{
+        #   'StoppingInstances': [
+        #        {
+        #            'CurrentState': {
+        #                'Code': 123,
+        #                'Name': 'pending'|'running'|'shutting-down'|'terminated'|'stopping'|'stopped'
+        #            },
+        #            'InstanceId': 'string',
+        #            'PreviousState': {
+        #                'Code': 123,
+        #                'Name': 'pending'|'running'|'shutting-down'|'terminated'|'stopping'|'stopped'
+        #            }
+        #        },
+        #    ]
+        #}
+        status = response['StoppingInstances'][0]['CurrentState']['Name']
+
+        await message.channel.send(f'new server status: {status}, weh')
     else:
         await message.channel.send('you are not an admin, weh')
 
